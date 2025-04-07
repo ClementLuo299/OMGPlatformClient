84fbdeef68dca863622567c077a2890092c3f0cf
30233609 <dylan.shiels@ucalgary.ca>
2025-04-06 20:24:52 -0600

    Started making the tests for Whist

---
+++ tests/gamelogic/WhistTests.java
@@ -0,0 +1,18 @@
+package gamelogic;
+
+import gamelogic.pieces.*;
+import org.junit.*;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Tests the functionality of Whist Games
+ *
+ * @authors Dylan Shiels
+ * @date April 6, 2025
+ */
+public class CardTests {
+
+}
