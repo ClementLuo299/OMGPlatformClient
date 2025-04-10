caceec0794245c133b2a517b6ebcd99ecefb574c
Iman Hamzat <iman.hamzat@ucalgary.ca>
2025-04-09 21:46:43 -0600

    updated ExperienceHandler.java

--- src/java/statistics/ExperienceHandler.java
+++ src/java/statistics/ExperienceHandler.java
@@ -1,4 +1,9 @@
 package statistics;
+package statistics;
+
+import gamelogic.Player;
+
+import java.util.List;
 
 /**
  * Handles the awarding of Points to Players at the conclusion of a Game
@@ -9,10 +14,28 @@ package statistics;
 public class ExperienceHandler {
     // ATTRIBUTES
 
+    // The list of players being given experience
+    List<Player> players;
+    // The Winner of the game
+    Player winner;
+    // The average plays for this Game
+    int avgPlays;
 
 
     // CONSTRUCTOR
 
+    /**
+     * Instantiates an Experience Handler to award Players for their Gameplay
+     *
+     * @param players The given List of Players who participated
+     * @param winner The given Winning Player of the Game
+     * @param avgPlays The integer Average Plays required to win the Game
+     */
+    public ExperienceHandler(List<Player> players, Player winner, int avgPlays) {
+        this.players = players;
+        this.winner = winner;
+        this.avgPlays = avgPlays;
+    }
 
 
     // GETTERS
@@ -24,6 +47,19 @@ public class ExperienceHandler {
 
 
     // METHODS
+
+
+    public int awardPoint() {
+        // Checks if the Player is at a levelling boundary
+
+
+
+        return 1;
+    }
+
+    public void awardParticipation(List<Player> players) {
+        // Goes through the Player List to award them one point
+    }
 
 
 }
