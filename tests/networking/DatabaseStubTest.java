7a535fedb7f10caf73024312ced6149eeec819bc
30194936 <muhammedhamza.tahir@ucalgary.ca>
2025-04-10 23:04:39 -0600

    test file reworked into proper file

---
+++ tests/networking/DatabaseStubTest.java
@@ -0,0 +1,10 @@
+package networking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseStubTest {
+
+
+}
