ae813066c53b9d464a419509e3cce52100e7f7b3
30234836 <clement.luo@ucalgary.ca>
2025-05-17 17:30:16 -0600

    added login service

---
+++ src/main/java/com/controllers/systems/LoginService.java
@@ -0,0 +1,20 @@
+package com.controllers.systems;
+
+/**
+ * Business logic for the login screen
+ *
+ * @authors Clement Luo
+ * @date May 17, 2025
+ */
+public class LoginService {
+    public void login(String username, String password) {
+        // Simple validation
+        if (username.isEmpty() || password.isEmpty()) {
+            //
+        }
+    }
+
+    public void guestLogin(String username) {
+        //
+    }
+}
