ad96ed411fae28111668228e181d4d47df32fff0
Clement Luo <clementluo299@gmail.com>
2025-05-31 23:22:01 -0600

    cleaned up code 3

--- src/main/java/com/MainApplication.java
+++ src/main/java/com/MainApplication.java
@@ -2,14 +2,9 @@ package com;
 
 import com.core.lifecycle.LifecycleManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
 
 /**
  * Main entry point of the application.
@@ -19,23 +14,7 @@ import java.util.Objects;
  */
 public class MainApplication extends Application {
 
-    @Override public void start(Stage primaryStage) throws IOException {
-        try {
-            // Debug resource loading
-            URL loginFxml = getClass().getResource("/fxml/Login.fxml");
-            if (loginFxml == null) {
-                System.err.println("Could not find Login.fxml");
-                System.err.println("Working Directory: " + System.getProperty("user.dir"));
-                throw new IOException("Login.fxml not found");
-            }
-            System.out.println("Found Login.fxml at: " + loginFxml);
-
-            LifecycleManager.start(primaryStage);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+    @Override public void start(Stage primaryStage) throws IOException { LifecycleManager.start(primaryStage); }
 
     @Override public void stop() { LifecycleManager.stop(); }
 
