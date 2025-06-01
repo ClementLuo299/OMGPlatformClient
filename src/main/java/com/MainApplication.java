01bba9fed1b3ec9698366a9b7cf268a0d992581f
Clement Luo <clementluo299@gmail.com>
2025-05-31 23:53:03 -0600

    cleaned up code 5

--- src/main/java/com/MainApplication.java
+++ src/main/java/com/MainApplication.java
@@ -11,6 +11,8 @@ import java.io.IOException;
  *
  * @authors Clement Luo, Fatin Abrar Ankon, Dylan Shiels, Zaman Dogar
  * @date March 17, 2025
+ * @edited May 31, 2025
+ * @since 1.0 SNAPSHOT
  */
 public class MainApplication extends Application {
 
